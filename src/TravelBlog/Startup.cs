2f0ae91aa5bbe854749899d909fb8732a6c63629
Robert Harries <robertpharries@gmail.com>
2017-02-09 13:57:20 +1100

    Initial Commit
    Created project.
    Setup to accept Angular redirection

---
+++ src/TravelBlog/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace TravelBlog
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //angular routing check
+            app.Use(async (context, next) =>
+            {
+                await next();
+                
+                if(context.Response.StatusCode == 404 && !Path.HasExtension(context.Request.Path.Value))
+                {
+                    context.Request.Path = new PathString("/index.html");
+                    await next();
+                }
+            });
+
+            //sets up the app to check for static file first
+            app.UseDefaultFiles();
+            //allows the app to use static files like index.html
+            app.UseStaticFiles();
+        }
+    }
+}
